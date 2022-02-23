d3c59f1c8227f27aa50909b2a20ffd3d475a0896
Chiedozie Okafor <you@example.com>
2022-02-23 13:01:24 +0100

    First commit

---
+++ server.py
@@ -0,0 +1,44 @@
+from ast import Pass
+from crypt import methods
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+todoData = []
+
+@app.route('/')
+def index():
+    return render_template("index.html", todos=todoData)
+
+@app.route('/create_todo', methods=["POST"])
+def create_todo():
+    new_todo = request.form.get("new_todo")
+    todoData.append(new_todo)
+    return redirect(url_for("index"))
+ 
+@app.route("/delete_todo/<todo_item>")
+def delete(todo_item):
+    todoData.remove(todo_item)
+    return redirect(url_for("index"))
+
+index_to_update=''
+@app.route("/Update/<todo_item>")
+def update(todo_item):
+    global index_to_update
+    index_to_update = todoData.index(todo_item)
+    return render_template('update.html', todo_item=todo_item)
+
+@app.route("/Update_item", methods= ['POST'])
+def update_item():
+
+    if request.method == 'POST':
+
+        new_item = request.form.get('new_item')
+        todoData[index_to_update] = new_item
+
+        return redirect(url_for('index'))
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
